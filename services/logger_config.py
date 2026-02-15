45a6344d4f4664695453c2e3fb41f81a034bd808
Manglam <manglamdubey11@gmail.com>
2026-02-15 13:42:29 +0530

    bug in log and db file fixed

--- services/logger_config.py
+++ services/logger_config.py
@@ -1,14 +1,18 @@
 import logging
 import os
 
-# ensure logs folder exists
-if not os.path.exists("../logs"):
-    os.makedirs("../logs")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
 logging.basicConfig(
-    filename="../logs/app.log",
+    filename=LOG_FILE,
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
