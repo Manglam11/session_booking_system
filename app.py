f4d4d17ecd160f38ddfbaf1b278ab580bd72adec
Manglam <manglamdubey11@gmail.com>
2026-02-15 10:06:18 +0530

    SQL booking system completed

--- app.py
+++ app.py
@@ -20,12 +20,43 @@ def create_table():
     time TIME NOT NULL
     )
     """)
-    print(cursor.fetchall())
     conn.commit()
     conn.close()
     print("Table created successfully.")
 
+def add_booking(name, email, date, time):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO bookings (name, email, date, time)
+        VALUES (?, ?, ?, ?)
+    """, (name, email, date, time))
+
+    conn.commit()
+    conn.close()
+
+    print("Booking added successfully")
+
+
+def get_all_bookings():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM bookings")
+    rows = cursor.fetchall()
+
+    conn.close()
+    return rows
+
+
 if __name__ == "__main__":
     create_table()
+    # add_booking("Manglam", "test@mail.com", "15-02-2026", "10:00")
+
+    # PRINT ALL BOOKINGS
+    data = get_all_bookings()
+    print("All bookings:", data)
+
     app.run(debug=True)
 
